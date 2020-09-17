60661c56f254c7b61941af003cc984016d7c2705
Rodrigo Benitez <rorobenm@gmail.com>
2020-09-17 09:15:15 -0400

    feat: Add Landing page

---
+++ portal/src/Home/Banner.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import QueueAnim from 'rc-queue-anim';
+import { Row, Col } from 'antd';
+import './static/home.css';
+import explorarDatos from '../assets/imgs/explorar_datos.svg';
+import comprasCovid from '../assets/imgs/compras_covid.svg';
+import { Link } from 'react-router-dom';
+
+export const page1 = [
+  {
+    img: explorarDatos,
+    href: `/explore`,
+    title: 'Explorar Datos',
+    description: '',
+  },
+  {
+    img: comprasCovid,
+    title: 'Compras COVID',
+    href: `/action`,
+    description: ''
+  },
+];
+
+function Banner(props) {
+  const children = page1.map((card, i) => (
+    <Col className="card-wrapper" key={i.toString()} md={12} xs={24}>
+      <a className="card" href={card.href}>
+        <img src={card.img} alt="" className="card-img-top" />
+        <div className="card-body">
+          <span className="description">{card.description}</span>
+          <span className="title">{card.title}</span>
+          <div className="button-wrapper"></div>
+          <Link to={card.href}>
+            <button className="ver-mas-button">Ver más</button>
+          </Link>
+        </div>
+      </a>
+    </Col>
+  ));
+  return (
+    <div className="banner-wrapper">
+      <QueueAnim className="banner-title-wrapper">
+        <p className="banner-text" key="content">
+          Este es un portal en el que vas a poder explorar <strong>Datos Abiertos</strong>,
+para realizar un <strong>control de los gastos del COVID-19</strong>
+        </p>
+      </QueueAnim>
+      <section className="page-wrapper info-banner">
+        <QueueAnim
+          component={Row}
+          type="bottom"
+          className="page row text-center"
+          delay={500}
+        >
+          <Col className="card-wrapper" key="info" md={20} xs={24}>
+            <div className="info-card">
+              <Col className="info-box">
+                <span className="description">1200</span>
+                <span className="title">Salarios Pagados</span>
+              </Col>
+              <Col className="info-box">
+                <span className="description">1200</span>
+                <span className="title">Contratos del 2020</span>
+              </Col>
+              <Col className="info-box">
+                <span className="description">1200</span>
+                <span className="title">Contratos COVID-19</span>
+              </Col>
+            </div>
+          </Col>
+        </QueueAnim>
+      </section>
+      <section className="page-wrapper page1">
+        <QueueAnim
+          component={Row}
+          type="bottom"
+          className="page row text-center"
+          delay={500}
+        >
+          {children}
+        </QueueAnim>
+      </section>
+    </div>
+  );
+}
+
+
+export default Banner;
--- portal/src/index.tsx
+++ portal/src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Header from './Home/Header';
+import Footer from './Home/Footer';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header/>
     <App />
+    <Footer/>
   </React.StrictMode>,
   document.getElementById('root')
 );
