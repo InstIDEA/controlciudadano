fe998a2de3722ae37d40f6024d52a87f1c81cfb5
Arturo Volpe <arturovolpe@gmail.com>
2020-10-09 19:34:37 -0300

    feat: Simplify code for datasources
    Signed-off-by: Arturo Volpe <arturovolpe@gmail.com>

--- portal/src/pages/Datasources.tsx
+++ portal/src/pages/Datasources.tsx
@@ -1,14 +1,13 @@
-import React, { useEffect, useMemo, useState } from 'react';
-import { Button, Input, PageHeader, Space, Table } from 'antd';
-import { DownloadOutlined, LinkOutlined } from '@ant-design/icons';
-import { useHistory, Link } from 'react-router-dom';
-import { filterRedashList, RedashAPI } from '../RedashAPI';
-import { StringParam, useQueryParam } from 'use-query-params';
-import { Header } from '../components/layout/Header';
+import React, {useEffect, useMemo, useState} from 'react';
+import {Button, Input, PageHeader, Space, Table} from 'antd';
+import {DownloadOutlined, LinkOutlined, SearchOutlined} from '@ant-design/icons';
+import {Link, useHistory} from 'react-router-dom';
+import {filterRedashList, RedashAPI} from '../RedashAPI';
+import {StringParam, useQueryParam} from 'use-query-params';
+import {Header} from '../components/layout/Header';
 import Footer from '../components/layout/Footer';
 import './Datasources.css';
-import { SearchOutlined } from '@ant-design/icons'
-import { DataSet } from '../Model';
+import {DataSet} from '../Model';
 
 export function DS() {
 
@@ -20,21 +19,7 @@ export function DS() {
     useEffect(() => {
         setWorking(true);
         new RedashAPI().getDataSets()
-            .then(d => {
-                setData(prev => [
-                    ...d.query_result.data.rows.map(row => ({
-                        id: row.id,
-                        files: row.files,
-                        institution: row.institution,
-                        name: row.name,
-                        description: row.description,
-                        kind: row.kind,
-                        base_url: row.base_url,
-                        last_update: row.last_update
-
-                    })),
-                ])
-            })
+            .then(d => setData(d.query_result.data.rows))
             .finally(() => setWorking(false));
     }, []);
 
@@ -47,22 +32,22 @@ export function DS() {
         <Header tableMode={true} searchBar={
             <div className="header-search-wrapper">
                 <Input.Search
-                    prefix={<SearchOutlined />}
+                    prefix={<SearchOutlined/>}
                     suffix={null}
                     placeholder="Buscar"
                     key="search_input"
                     defaultValue={query || ''}
                     onSearch={v => setQuery(v)}
-                    style={{ width: 200 }}
-                    formMethod="submit" />
+                    style={{width: 200}}
+                    formMethod="submit"/>
             </div>
-        } />
+        }/>
         <PageHeader title="Fuente"
-            onBack={() => history.push('/')}
-            backIcon={null}
-            subTitle="">
+                    onBack={() => history.push('/')}
+                    backIcon={null}
+                    subTitle="">
 
-            <div style={{ padding: 12 }}>
+            <div style={{padding: 12}}>
                 <Table<DataSet>
                     loading={working}
                     rowKey="url"
@@ -76,11 +61,11 @@ export function DS() {
                         dataIndex: "name",
                         render: (_, row) => <Space className="action-column">
                             <Space>
-                                {LinkToDS({ data: row })}
+                                {LinkToDS({data: row})}
                                 {row.base_url && <a href={row.base_url} target="_blank" rel="noopener noreferrer">
-                                    <Button className="btn-wrapper btn-secondary" icon={<LinkOutlined />}>
-                                        Ir a fuente
-                            </Button>
+                                  <Button className="btn-wrapper btn-secondary" icon={<LinkOutlined/>}>
+                                    Ir a fuente
+                                  </Button>
                                 </a>}
                             </Space>
                         </Space>
@@ -92,23 +77,24 @@ export function DS() {
                 />
             </div>
         </PageHeader>
-        <Footer tableMode={true} />
+        <Footer tableMode={true}/>
     </>
 }
 
 function LinkToDS(props: { data: DataSet }) {
-    if (props.data.files) return <Link to={`/sources/${props.data.id}`} >
-        <Button type="primary" className="btn-wrapper" icon={<DownloadOutlined />}>
+    if (props.data.files) return <Link to={`/sources/${props.data.id}`}>
+        <Button type="primary" className="btn-wrapper" icon={<DownloadOutlined/>}>
             Ver más
         </Button>
     </Link>
     if (props.data.id === 9) return <Link to="/action">
-        <Button type="primary" className="btn-wrapper" icon={<DownloadOutlined />}>
-            Ver más
+        <Button type="primary" className="btn-wrapper" icon={<SearchOutlined/>}>
+            Explorar datos
         </Button>
     </Link>
-    if (props.data.id === 10) return <Link to="/explore/contralory/affidavit"><Button type="primary" className="btn-wrapper" icon={<DownloadOutlined />}>
-        Ver más
-    </Button></Link>
+    if (props.data.id === 10) return <Link to="/explore/contralory/affidavit">
+        <Button type="primary" className="btn-wrapper" icon={<SearchOutlined/>}>
+            Explorar datos
+        </Button></Link>
     return <></>
 }
