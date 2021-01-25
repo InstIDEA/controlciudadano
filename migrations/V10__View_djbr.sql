cbe8a47155bb5045893b48719086315db17ebee9
Arturo Volpe <arturovolpe@gmail.com>
2021-01-25 19:30:24 -0300

    feat: Add script to create the analysis.djbr view
    This view will be the main source of all the declarations, it's a UNION
    between the two version of the data.
    
    Signed-off-by: Arturo Volpe <arturovolpe@gmail.com>

---
+++ migrations/V10__View_djbr.sql
@@ -0,0 +1,46 @@
+CREATE MATERIALIZED VIEW IF NOT EXISTS analysis.djbr AS
+(
+(SELECT raw.id,
+        CAST(raw.cedula as TEXT)                                                                        as document,
+        raw.nombres                                                                                     as name,
+        raw.periodo                                                                                     as year,
+        10000                                                                                           as version,
+        CASE
+            WHEN file is not null THEN 'https://data.controlciudadanopy.org/contraloria/declaraciones/' ||
+                                       file.file_name
+            END                                                                                         as link,
+        'https://portaldjbr.contraloria.gov.py/portal-djbr/api/consulta/descargarpdf/' || raw.remote_id as origin,
+        NULL                                                                                            as link_sandwich,
+        'V2'                                                                                            as type,
+        NULL                                                                                            as active,
+        NULL                                                                                            as passive,
+        NULL                                                                                            as net_worth,
+        NULL                                                                                            as scrapped_data,
+        NULL                                                                                            as charge,
+        CASE
+            WHEN file is not null THEN file.download_date
+            END                                                                                         as downloaded_data
+ FROM staging.djbr_raw_data raw
+          LEFT JOIN staging.djbr_downloaded_files file ON raw.id = file.raw_data_id
+--  LIMIT 10
+)
+UNION
+(SELECT -id,
+        document,
+        name,
+        year,
+        version,
+        regexp_replace(link, ' ', '', 'g')          as link,
+        regexp_replace(origin, ' ', '', 'g')        as origin,
+        regexp_replace(link_sandwich, ' ', '', 'g') as link_sandwich,
+        'V1'                                        as type,
+        active,
+        passive,
+        net_worth,
+        scrapped_data,
+        charge,
+        download_date
+ FROM analysis.declarations
+--  LIMIT 10
+)
+);
