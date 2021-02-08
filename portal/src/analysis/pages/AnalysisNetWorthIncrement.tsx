040eb95c430bd502bc438556e0220a8a5fe1ca8b
Arturo Volpe <arturovolpe@gmail.com>
2021-02-08 17:58:50 -0300

    feat: Add calculation in NetWorth analysis
    Signed-off-by: Arturo Volpe <arturovolpe@gmail.com>

--- portal/src/analysis/pages/AnalysisNetWorthIncrement.tsx
+++ portal/src/analysis/pages/AnalysisNetWorthIncrement.tsx
@@ -1,31 +1,36 @@
-import React from "react";
+import React, {useCallback, useState} from "react";
 import {Header} from "../../components/layout/Header";
 import Footer from "../../components/layout/Footer";
 import {Button, Card, Col, Layout, Row, Space, Typography} from "antd";
 import {useParams} from "react-router-dom";
 import {DownloadOutlined} from "@ant-design/icons";
 import {DisclaimerComponent} from "../../components/Disclaimer";
+import {DeclarationData, NetWorthIncrementData} from "../AnalysisModel";
+import {Calculations} from "../components/net_worth/Calculations";
+import {InputData} from "../components/net_worth/InputData";
+import {Graphs} from "../components/net_worth/Graphs";
 
 
 export function AnalysisNetWorthIncrement() {
 
     const {document} = useParams<{ document: string }>();
+    const data = useDeclarationData(document);
 
     return <>
         <Header/>
 
         <Layout>
-            <Layout.Content style={{minHeight: '75vh', padding: '10px 5%'}}>
+            <Layout.Content style={{padding: '0 30px'}}>
                 <Row gutter={[16, 16]} justify="center">
-                    <Col xs={24}>
-                        <Row align="middle">
-                            <Col xs={22} md={{span: 16, offset: 4}}>
+                    <Col xs={24} xl={18}>
+                        <Row align="middle" justify="center">
+                            <Col xs={22}>
                                 <Typography.Title className="title-color" style={{textAlign: 'center'}}>
                                     Crecimiento Patrimonial de {document} según Declaraciones Juradas de Bienes y
                                     Rentas.
                                 </Typography.Title>
                             </Col>
-                            <Col xs={{span: 1, offset: 1}}>
+                            <Col md={{span: 1, offset: 1}} xs={{span: 13, offset: 11}}>
                                 <Button type="primary"
                                         shape="circle"
                                         size="large"
@@ -34,22 +39,22 @@ export function AnalysisNetWorthIncrement() {
                             </Col>
                         </Row>
                     </Col>
-                    <Col xs={24}>
+                    <Col xs={24} xl={{span: 18}}>
                         <Card className="custom-card custom-shadow-small">
-                            <Graphs/>
+                            <Graphs data={data.data}/>
                         </Card>
                     </Col>
-                    <Col md={12} sm={24}>
+                    <Col md={12} sm={24} xl={9}>
                         <Card className="custom-card custom-shadow-small">
-                            <Calculations/>
+                            <Calculations data={data.data}/>
                         </Card>
                     </Col>
-                    <Col md={12} sm={24}>
+                    <Col md={12} sm={24} xl={9}>
                         <Card className="custom-card custom-shadow-small">
-                            <InputData/>
+                            <InputData data={data.data} updateDate={data.setYearData}/>
                         </Card>
                     </Col>
-                    <Col sm={24}>
+                    <Col sm={24} xl={18}>
                         <DisclaimerComponent full card>
                             <Space>
                                 <Typography.Paragraph style={{margin: 'inherit'}}>
@@ -66,37 +71,57 @@ export function AnalysisNetWorthIncrement() {
     </>
 }
 
-export function Graphs() {
-    return <Row justify="center">
-        <Col md={12} sm={24}>
-            <Typography.Title level={5} className="title-color">
-                Crecimiento Patrimonial
-            </Typography.Title>
-        </Col>
-        <Col md={12} sm={24}>
-            <Typography.Title level={5} className="title-color">
-                Crecimiento salarial
-            </Typography.Title>
-        </Col>
-    </Row>
+function useDeclarationData(document: string) {
+
+    const [data, setData] = useState<NetWorthIncrementData>({
+        person: {
+            document,
+            name: 'Cargando...'
+        },
+        duration: 60,
+        firstYear: calculateData({
+            otherActives: 1200000000,
+            income: 10000000,
+            active: 0,
+            activeDetails: [],
+            netWorth: 0,
+            passive: 18000000,
+            year: 2015
+        }),
+        lastYear: calculateData({
+            otherActives: 1400000000,
+            income: 15000000,
+            active: 0,
+            activeDetails: [],
+            netWorth: 0,
+            passive: 14000000,
+            year: 2020
+        })
+    })
+
+    const setYearData = useCallback((newData: DeclarationData) => {
+        setData(d => ({
+            ...d,
+            firstYear: newData.year === d.firstYear.year ? calculateData(newData) : d.firstYear,
+            lastYear: newData.year === d.lastYear.year ? calculateData(newData) : d.lastYear
+        }))
+
+    }, []);
+
+    return {
+        data,
+        setYearData
+    }
 }
 
-export function Calculations() {
-    return <Row justify="center">
-        <Col sm={24}>
-            <Typography.Title level={5} className="title-color">
-                Análisis
-            </Typography.Title>
-        </Col>
-    </Row>
-}
 
-export function InputData() {
-    return <Row justify="center">
-        <Col sm={24}>
-            <Typography.Title level={5} className="title-color">
-                Datos
-            </Typography.Title>
-        </Col>
-    </Row>
+function calculateData(data: DeclarationData) {
+
+    const active = data.income + data.otherActives;
+    const netWorth = active - data.passive;
+    return {
+        ...data,
+        active,
+        netWorth
+    }
 }
