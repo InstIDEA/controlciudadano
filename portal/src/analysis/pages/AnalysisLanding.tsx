6ea1d70a43a2cffd44f0de266d085cee2e7fd761
Arturo Volpe <arturovolpe@gmail.com>
2021-02-24 15:45:27 -0300

    feat: Add carousel to analysis landing
    Signed-off-by: Arturo Volpe <arturovolpe@gmail.com>

--- portal/src/analysis/pages/AnalysisLanding.tsx
+++ portal/src/analysis/pages/AnalysisLanding.tsx
@@ -1,9 +1,33 @@
 import React, {PropsWithChildren} from "react";
 import {Header} from "../../components/layout/Header";
 import Footer from "../../components/layout/Footer";
-import {Card, Col, Input, Layout, Result, Row, Typography} from "antd";
+import {Card, Carousel, Col, Input, Layout, Result, Row, Typography} from "antd";
 import {Link} from "react-router-dom";
 import inProgressIcon from "../../assets/imgs/analysis_in_progress.svg";
+import './AnalysisLanding.css';
+
+const DBJRText = [{
+    title: 'Análisis de declaraciones juradas de bienes y rentas',
+    description: 'La herramienta de análisis de declaraciones juradas, es una plataforma tecnológica que permite generar información preliminar sobre la razonabilidad del crecimiento patrimonial de los funcionarios públicos, comparando los datos de los activos, pasivos e ingresos declarados por los funcionarios públicos en sus declaraciones juradas de bienes y rentas presentadas que se publican en la página de la <a href="https://portaldjbr.contraloria.gov.py/portal-djbr/"> Contraloría General de la República </a>.'
+}, {
+    title: '¿Cómo se alimenta la plataforma?',
+    description: 'La herramienta utiliza un proceso de extracción de datos de distintas fuentes, siendo la\n                            principal el portal de declaraciones juradas de la Contraloría General de la República, una\n                            vez obtenidos los documentos, se realiza una tarea de extracción de información, en la cual\n                            se extraen los datos más relevantes de una declaración y se guardan en un formato de datos\n                            abiertos'
+}, {
+    title: '¿Qué puedo hacer si detecto una inconsistencia alta?',
+    description: 'Cada análisis generado es de exclusiva responsabilidad del usuario. El mismo,  puede enviar una nota formal dirigida a la CGR, mediante sus canales institucionales, a fin de que esta institución tome conocimiento y proceda a realizar el análisis formal de correspondencia sobre las declaraciones juradas analizadas.\n' +
+        '<br /><br />Véase <a href="https://www.contraloria.gov.py/">https://www.contraloria.gov.py/</a>'
+}];
+
+
+const DBJRExamples = shuffle([
+    '3335266',
+    '4069817',
+    '583872',
+    '3851730',
+    '3363139',
+    '3435873',
+    '3826933',
+]).slice(0, 3);
 
 
 export function AnalysisLanding() {
@@ -13,6 +37,7 @@ export function AnalysisLanding() {
 
         <Layout>
             <Layout.Content style={{minHeight: '75vh', padding: '0 5%'}}>
+
                 <Row gutter={[16, 16]} style={{flexDirection: 'column'}}>
                     <Col>
                         <Typography.Title style={{textAlign: 'center'}} className="title-color">
@@ -20,28 +45,13 @@ export function AnalysisLanding() {
                         </Typography.Title>
                     </Col>
                     <Col>
-                        <Typography.Paragraph style={{textAlign: 'center'}}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nibh elit, eleifend
-                            laci-nia bibendum sed, placerat sed sem. Sed vitae elit nibh. Maecenas non posuere justo,
-                            varius
-                            accumsan eros. Mauris vel dui at metus rutrum varius. Aliquam vitae est ac augue tristique
-                            cursus. Morbi cursus tellus non placerat rhoncus. Nulla vehicula ligula justo, sed viverra
-                            sem
-                            gravida quis. Pellentesque ornare.
-                        </Typography.Paragraph>
-                    </Col>
-                    <Col>
                         <Analysis videoUrl="https://www.youtube.com/embed/C_LruTtdrgo"
-                                  title="Crecimiento patrimonial"
-                                  description="Lorem  ipsum  dolor  sit  amet,  consectetur  adipiscing  elit.  Donec  eu  faucibus  odio,  vitae  elementum  tortor.  Suspendisse  ut  velit  tincidunt,  sollicitudin  tortor  eget,  venenatis   leo.   Aenean   venenatis   purus   dolor,   eget   mollis."
-                        >
+                                  name="Crecimiento patrimonial"
+                                  parts={DBJRText}>
                             <Row gutter={[8, 8]} justify="center">
-                                <Col>
-                                    <LinkButton linkTo="/analysis/net_worth/123456" text="Ejemplo 1"/>
-                                </Col>
-                                <Col>
-                                    <LinkButton linkTo="/analysis/net_worth/492599" text="Ejemplo 2"/>
-                                </Col>
+                                {DBJRExamples.map((ex, idx) => <Col key={ex}>
+                                    <LinkButton linkTo={`/analysis/net_worth/${ex}`} text={`Ejemplo ${idx + 1}`}/>
+                                </Col>)}
                                 <Col>
                                     <Input style={{width: '100%', borderRadius: 5}}
                                            placeholder="Ingrese una cédula"/>
@@ -67,33 +77,52 @@ export function AnalysisLanding() {
                 </Row>
             </Layout.Content>
         </Layout>
+
         <Footer/>
     </>
 }
 
 
 function Analysis(props: PropsWithChildren<{
-    videoUrl: string,
-    title: string,
-    description: string,
+    videoUrl: string;
+    name: string;
+    parts: { title: string, description: string }[];
 }>) {
+
     return <Card className="custom-card">
         <Row style={{minHeight: 512}} gutter={[16, 16]}>
 
+
             <Col md={12} sm={24}>
-                <iframe title={props.title} src={props.videoUrl} width="100%" height="100%"/>
+                <iframe title={props.name} src={props.videoUrl} width="100%" height="100%"/>
             </Col>
             <Col md={12} sm={24}>
                 <Row gutter={[8, 8]} justify="center">
-                    <Col>
-                        <Typography.Title className="title-color">
-                            {props.title}
-                        </Typography.Title>
-                    </Col>
-                    <Col>
-                        <Typography.Paragraph>
-                            {props.description}
-                        </Typography.Paragraph>
+                    <Col xs={24}>
+                        <Carousel dotPosition="top" adaptiveHeight dots>
+                            {props.parts.map(part => <div key={part.title}>
+                                    <Row style={{
+                                        minHeight: 200,
+                                        paddingTop: 20,
+                                        // color: '#fff',
+                                        // lineHeight: 100,
+                                        textAlign: 'left',
+                                        // background: '#364d79',
+                                    }}>
+                                        <Col>
+                                            <Typography.Title className="title-color">
+                                                {part.title}
+                                            </Typography.Title>
+                                        </Col>
+                                        <Col>
+                                            <Typography.Paragraph>
+                                                <div dangerouslySetInnerHTML={{__html: part.description}}/>
+                                            </Typography.Paragraph>
+                                        </Col>
+                                    </Row>
+                                </div>
+                            )}
+                        </Carousel>
                     </Col>
                     <Col>
                         {props.children}
@@ -115,3 +144,11 @@ function LinkButton(props: {
     </Link>
 }
 
+
+function shuffle<T>(array: Array<T>): Array<T> {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
