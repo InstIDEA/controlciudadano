f64fa768c8cbe256c6bb3322bcb8dd33ab227d56
Arturo Volpe <arturovolpe@gmail.com>
2021-03-06 17:39:14 -0300

    feat: Add api test and endpoint to get a single dec
    Signed-off-by: Arturo Volpe <arturovolpe@gmail.com>

---
+++ api/src/analysis/NetWorthAnalysis.test.ts
@@ -0,0 +1,80 @@
+import {expect} from 'chai';
+import {ContextData, NetWorthAnalysis, NetWorthAnalysisEnhancer} from "./NetWorthAnalysis";
+import {AnalysisDJBR} from "../APIModel";
+
+describe('NetWorthAnalysisTest', () => { // the tests container
+    it('Use the latest available', () => { // the single test
+        const toTest = new NetWorthAnalysis(); // this will be your class
+
+        const decs: Array<AnalysisDJBR> = [
+            getDec({download_date: '2020/01/01 12:40', id: 20, year: 2020}),
+            getDec({download_date: '2020/01/02 12:40', id: 19, year: 2020}),
+        ]
+
+        expect(toTest.getLatestDeclarationsPerYear(decs)[2020].id).to.be.eq(19);
+
+    });
+
+    it('Fetch two sequential', () => { // the single test
+        const toTest = new NetWorthAnalysis(); // this will be your class
+
+        const decs: Array<AnalysisDJBR> = [
+            getDec({download_date: '2020/01/01 12:40', id: 20, year: 2020}),
+            getDec({download_date: '2020/01/02 12:40', id: 19, year: 2020}),
+            getDec({download_date: '2020/01/01 12:40', id: 18, year: 2019}),
+            getDec({download_date: '2020/01/02 12:40', id: 17, year: 2019}),
+        ]
+
+        const grouped = toTest.getLatestDeclarationsPerYear(decs);
+
+        const best = toTest.getBestDeclarations(grouped);
+
+        expect(best.first.year).to.be.eq(2020);
+        expect(best.first.id).to.be.eq(19);
+        expect(best.last.year).to.be.eq(2019);
+        expect(best.last.id).to.be.eq(17);
+
+    });
+
+    it('Fetch single year', async () => { // the single test
+        const toTest = new NetWorthAnalysis([new NoOpEnhancer()]); // this will be your class
+
+        const dec = getDec({download_date: '2020/01/02 12:40', id: 17, year: 2019});
+
+
+        const best = await toTest.getSpecificYear(dec);
+
+        expect(best).to.be.exist;
+        expect(best.year).to.be.eq(2019);
+
+    });
+});
+
+
+function getDec(base: Partial<AnalysisDJBR>): AnalysisDJBR {
+    return {
+        id: 19,
+        download_date: '2020/01/01 12:40',
+        year: 2014,
+        link: '',
+        origin: '',
+        name: '',
+        document: '',
+        net_worth: '',
+        active: '',
+        passive: '',
+        charge: '',
+        link_sandwich: '',
+        scrapped_data: {},
+        type: '',
+        version: 2,
+        ...base
+    }
+}
+
+
+class NoOpEnhancer implements NetWorthAnalysisEnhancer {
+    enhance(context: ContextData): Promise<void> {
+        return
+    }
+}
--- api/src/services/AnalysisService.ts
+++ api/src/services/AnalysisService.ts
@@ -1,6 +1,7 @@
 import {Pool} from "pg";
-import {AnalysisDJBR, NetWorthIncreaseAnalysis} from "../APIModel";
+import {AnalysisDJBR, DeclarationData, NetWorthIncreaseAnalysis} from "../APIModel";
 import {NetWorthAnalysis} from "../analysis/NetWorthAnalysis";
+import {ApiError} from "../index";
 
 export class AnalysisService {
 
@@ -10,12 +11,40 @@ export class AnalysisService {
 
     async netWorthIncrement(document: string): Promise<NetWorthIncreaseAnalysis> {
 
-        return new NetWorthAnalysis().buildData(await this.fetchDeclarations(document), document);
+        const found = await this.fetchDeclarationsOf(document);
+
+        if (found.length === 0 || found[0].document !== document) {
+            throw new ApiError(`User with document ${document} not found`, 404, {
+                document
+            });
+        }
+        return new NetWorthAnalysis().buildData(await this.fetchDeclarationsOf(document), document);
     }
 
-    private async fetchDeclarations(document: string): Promise<Array<AnalysisDJBR>> {
+    async getDataOfYear(document: string, id: number): Promise<DeclarationData> {
+
+        const found = await this.fetchDeclaration(id);
+
+
+        if (found.length === 0 || found[0].document !== document) {
+            throw new ApiError(`Declaration with id ${id} of ${document} not found`, 404, {
+                document,
+                id
+            });
+        }
+
+        return new NetWorthAnalysis().getSpecificYear(found[0]);
+    }
+
+    private async fetchDeclarationsOf(document: string): Promise<Array<AnalysisDJBR>> {
 
         const data = await this.pool.query("SELECT * FROM analysis.djbr WHERE document = $1", [document])
+        return data.rows;
+    }
+
+    private async fetchDeclaration(id: number): Promise<Array<AnalysisDJBR>> {
+
+        const data = await this.pool.query("SELECT * FROM analysis.djbr WHERE id = $1", [id])
         return data.rows;
     }
 }
