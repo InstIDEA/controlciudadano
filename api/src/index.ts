cef32de5c1bfb1f73624e70415fe61e326602a69
Arturo Volpe <arturovolpe@gmail.com>
2021-03-07 21:08:12 -0300

    feat: Add a service for dynamic queries
    This service is heavily inspired in jQuery datatables, but the protocol
    is simpler.
    
    Currently it support a limit kind of where (and of various columns),
    complex sorting (multiple columns) and pagination (offset/limit).
    
    This class has two TODOs:
    
    1. At startup check all the columns of the given table, to ensure that
    no external table is queried.
    2. Receive the express req to allow more flexibility at the parsing of
    parameters.
    
    Signed-off-by: Arturo Volpe <arturovolpe@gmail.com>

--- api/src/index.ts
+++ api/src/index.ts
@@ -7,6 +7,7 @@ import helmet from 'helmet';
 import {ContraloryService} from './services/ContraloryService';
 import {AnalysisService} from "./services/AnalysisService";
 import {AdminOperationsService} from "./services/AdminOperationsService";
+import {ColumnConfig, DataTablesService} from "./datatables/DataTablesService";
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -17,6 +18,7 @@ if (!process.env.API_KEY) {
 }
 
 const pool = new pg.Pool()
+const dtService = new DataTablesService(pool);
 
 app.use(cors())
 app.use(helmet());
@@ -47,8 +49,33 @@ app.get('/api/people/:document/declarations', wrap(req => {
 }));
 
 app.get('/api/contralory/declarations', wrap(req => {
-    const page = extractPage(req);
-    return new ContraloryService(pool).getDeclarations(page.size, page.page);
+    const columns: ColumnConfig[] = [
+        {name: 'id', filterType: "no_filter"},
+        {name: 'document', filterType: 'equals'},
+        {name: 'name', filterType: 'contain'},
+        {name: 'year', filterType: 'equals'},
+        {name: 'version', filterType: 'equals'},
+        {name: 'link', filterType: 'no_filter'},
+        {name: 'origin', filterType: 'no_filter'},
+        {name: 'link_sandwich', filterType: 'no_filter'},
+        {name: 'type', filterType: 'no_filter'},
+        {name: 'active', filterType: 'equals', descNullLast: true},
+        {name: 'passive', filterType: 'equals', descNullLast: true},
+        {name: 'net_worth', filterType: 'equals', descNullLast: true},
+        {name: 'charge', filterType: 'equals'},
+    ];
+    return dtService.query({
+        columns,
+        table: 'analysis.djbr',
+        idColumn: 'id',
+        baseOrder: {
+            'net_worth': 'DESC'
+        }
+    }, {
+        ...extractPage(req),
+        filter: dtService.extractColumnFilter(req.query, columns),
+        order: dtService.extractSortOrder(req.query, columns)
+    });
 }));
 
 app.get('/api/ocds/items', wrap(req => {
@@ -151,19 +178,19 @@ async function handlePromise(
     }
 }
 
-function extractPage(req: express.Request) {
-    const page = toNumber(req.query.page);
-    const size = toNumber(req.query.size);
+function extractPage(req: express.Request): { page: number, size: number } {
+    const page = toNumber(req.query.page || '1');
+    const size = toNumber(req.query.size || '10');
     if (!assertNumber(page) || !assertNumber(size)) {
-        throw new ApiError('invalid.pagination', 400, {page, size});
+        throw new ApiError('invalid.pagination', 400, {page: req.query.page, size: req.query.size});
     }
 
     return {page, size}
 }
 
-function handleError(err: unknown, res: express.Response) {
+function handleError(err: object, res: express.Response) {
     if (err instanceof ApiError) {
-        console.warn('Error in API, check console ', err.meta, err);
+        console.warn('ApiError in API, check console ', err.meta, err);
         res.status(err.code || 500).send({reason: err.message || 'Unexpected error', meta: err.meta})
     } else {
         console.warn('Error in API, check console ', err);
@@ -172,7 +199,7 @@ function handleError(err: unknown, res: express.Response) {
 }
 
 function assertNumber(val: any): val is number {
-    return typeof val === 'number';
+    return typeof val === 'number' && !isNaN(val);
 
 }
 
@@ -191,6 +218,12 @@ export class ApiError extends Error {
 function validateNonEmpty(paramName: string, param: string): string {
     if (!param || param.trim() === '') throw new ApiError(`invalid.${paramName}`)
     return param;
+}
+
+function validateNonEmptyString(paramName: string, param: unknown): string {
+    const str = validateString(paramName, param);
+    if (!str || str.length === 0) throw new ApiError(`invalid.${paramName}`)
+    return str;
 }
 
 function validateString(paramName: string, param: any): string {
